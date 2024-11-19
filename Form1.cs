0110eef46ccc65c2c0850035114d8d4cd0316d09
joelpaulin0 <a7154@esap.edu.pt>
2024-11-19 17:01:09 +0000

    CRUD a Colaboradores

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,11 @@ namespace ReqFerramentas
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmColaboradores frm = new FrmColaboradores(); 
+            frm.ShowDialog();
+        }
     }
 }
